7fd999f2587048d0cbe10812a8ab00999092ad8c
ray <raypark970421@gmail.com>
2023-03-01 20:24:26 +0900

    chore() : 환경변수 validation scheme

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrmConfig } from '../orm-config';
 import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
   imports: [
@@ -16,6 +17,14 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'development' && '.dev.env',
       ignoreEnvFile: process.env.NODE_ENV === 'production',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid(['development', 'production']),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRoot(OrmConfig),
     GraphQLModule.forRoot({
